b84b996aaeb8952199b6abff7153f09c9b81fbd1
laura Meneses <lmeneses58@gmail.com>
2021-05-12 20:35:08 -0400

    refactoring

--- text_classifier/__init__.py
+++ text_classifier/__init__.py
@@ -1,4 +1,4 @@
-import schedule
+# import schedule
 import time
 
 from .extraction import execute as execute_extraction
--- text_classifier/extraction.py
+++ text_classifier/extraction.py
@@ -6,40 +6,16 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
-# FIXME por qué no se usan estas constantes?
-
-
 LOS_TIEMPOS_BASE_URL = "https://www.lostiempos.com/"
-Opinion = "https://www.opinion.com.bo/blog/section/cochabamba"
-LaRazon = "https://www.la-razon.com/sociedad/"  # FIXME por qué esta sección y no otra?
+OPINION_BASE_URL = "https://www.opinion.com.bo/blog/section/cochabamba"
+LA_RAZON_BASE_URL = "https://www.la-razon.com/nacional/"
 
 
 def execute():
     print("Iniciando el proceso de extracción en {}".format(os.getenv("TEXT_CLASSIFIER_DATA")))
     __extract_text(LOS_TIEMPOS_BASE_URL)
-    #     print("text extraction to LosTiempos".format(os.getenv('TEXT_CLASSIFIER_DATA')));
-    __extract_text(Opinion)
-    #     print("text extraction to Opinion".format(os.getenv('TEXT_CLASSIFIER_DATA')));
-    __extract_text(LaRazon)
-
-
-#     print("text extraction to LaRazon".format(os.getenv('TEXT_CLASSIFIER_DATA')));
-
-# def __get_divs_LosTiempos(page):
-#     articles_divs1 = page.body.find_all(name="div", attrs={"class": "views-field-title"})
-#     articles_divs2 = page.body.find_all(name="div", attrs={"class": "views-field-title term"})
-#     articles_divs = articles_divs1 + articles_divs2
-#     return articles_divs
-
-
-def __get_divs_Opinion(page):
-    articles_divs = page.body.find_all(name="h2", attrs={"class": "title title-article"})
-    return articles_divs
-
-
-def __get_divs_la_razon(page):
-    articles_divs = page.body.find_all(name="a", attrs={"class": "title"})
-    return articles_divs
+    __extract_text(OPINION_BASE_URL)
+    __extract_text(LA_RAZON_BASE_URL)
 
 
 def __div_links(articles_divs):
@@ -57,40 +33,6 @@ def __get_links(articles_divs):
     return links
 
 
-def __get_links_la_razon(articles_divs):
-    links = []
-    for article in articles_divs:
-        link = article.get("href")
-        links.append(link)
-    return links
-
-
-def __get_fileName_LosTiempos(date_div, title_article):
-    article_date = [date.text for date in date_div]
-    match = re.search(r'\d{2}/\d{2}/\d{4}', article_date[0])
-    date = datetime.strptime(match.group(), '%d/%m/%Y')
-    dateStr = date.strftime("%d %b %Y ")
-    article_title = (dateStr + title_article)
-    article_title = __remove_punctuation(article_title)
-    return article_title
-
-
-def __get_fileName_Opinion(date_div, title_article):
-    article_date = [date.text for date in date_div]
-    article_title = (article_date[0] + title_article)
-    article_title = __remove_punctuation(article_title)
-    return article_title
-
-
-def __get_fileName_LaRazon(date_div, title_article):
-    match = re.search(r'\d{4}/\d{2}/\d{2}', date_div)
-    date = datetime.strptime(match.group(), '%Y/%m/%d')
-    dateStr = date.strftime("%d %b %Y ")
-    article_title = (dateStr + title_article)
-    article_title = __remove_punctuation(article_title)
-    return article_title
-
-
 def __remove_punctuation(text):
     return re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
 
@@ -98,83 +40,121 @@ def __remove_punctuation(text):
 def __download_article(title, article):
     path = os.getenv("TEXT_CLASSIFIER_DATA") + title
     print("Guardando artículo en {}".format(path))
-    f = open(path, "a+")  # FIXME por qué no usar 'article_file' en lugar de 'f'?
+    article_file = open(path, "a+")
     for paragraph in article:
-        f.write(paragraph + "\n")
-    f.close()
+        article_file.write(paragraph + "\n")
+    article_file.close()
 
 
-def __get_articles_LosTiempos(links):
-    for link in links:
-        response = requests.get("https://www.lostiempos.com" + link)
-        page = BeautifulSoup(response.text, "html.parser")
-        article_div = page.body.find_all(name="p", attrs={"class": "rtejustify"})
-        date_div = page.body.find_all(name="div", attrs={"class": "date-publish"})
-        if len(date_div) > 0:
-            title_article = page.title.text
-            file_name = __get_fileName_LosTiempos(date_div, title_article) + ".txt"
+def __extract_los_tiempos(page):
+
+    def __get_divs_los_tiempos(page_div):
+        articles_divs1 = page_div.body.find_all(name="div", attrs={"class": "views-field-title"})
+        articles_divs2 = page_div.body.find_all(name="div", attrs={"class": "views-field-title term"})
+        article_divs = articles_divs1 + articles_divs2
+        return article_divs
+
+    def __get_file_name_los_tiempos(date_div, title_article):
+        article_date = [date.text for date in date_div]
+        match = re.search(r'\d{2}/\d{2}/\d{4}', article_date[0])
+        date = datetime.strptime(match.group(), '%d/%m/%Y')
+        date_str = date.strftime("%d %b %Y ")
+        article_title = (date_str + title_article)
+        article_title = __remove_punctuation(article_title)
+        return article_title
+
+    def __get_articles_los_tiempos(articles_links):
+        for link in articles_links:
+            response = requests.get("https://www.lostiempos.com" + link)
+            article_page = BeautifulSoup(response.text, "html.parser")
+            article_div = article_page.body.find_all(name="p", attrs={"class": "rtejustify"})
+            date_div = article_page.body.find_all(name="div", attrs={"class": "date-publish"})
+            if len(date_div) > 0:
+                title_article = article_page.title.text
+                file_name = __get_file_name_los_tiempos(date_div, title_article) + ".txt"
+                article_text = [article.get_text().strip() for article in article_div]
+                __download_article(file_name, article_text)
+            else:
+                pass
+        print("Done")
+
+    articles_divs = __get_divs_los_tiempos(page)
+    links = __get_links(articles_divs)
+    __get_articles_los_tiempos(links)
+
+
+def __extract_opinion(page):
+
+    def __get_divs_opinion(page_div):
+        article_divs = page_div.body.find_all(name="h2", attrs={"class": "title title-article"})
+        return article_divs
+
+    def __get_file_name_opinion(date_div, title_article):
+        article_date = [date.text for date in date_div]
+        article_title = (article_date[0] + title_article)
+        article_title = __remove_punctuation(article_title)
+        return article_title
+
+    def __get_articles_opinion(articles_links):
+        for link in articles_links:
+            response = requests.get("https://www.opinion.com.bo" + link)
+            article_page = BeautifulSoup(response.text, "html.parser")
+            date_div = article_page.body.find_all(name="span", attrs={"class": "content-time"})
+            article_div = article_page.body.find_all(name="p", attrs={})
+            title_article = article_page.title.text
+            file_name = __get_file_name_opinion(date_div, title_article) + ".txt"
             article_text = [article.get_text().strip() for article in article_div]
             __download_article(file_name, article_text)
-        else:
-            pass
-    print("Done")
+        print("Done")
 
-
-def __get_articles_Opinion(links):
-    for link in links:
-        response = requests.get("https://www.opinion.com.bo" + link)
-        page = BeautifulSoup(response.text, "html.parser")
-        date_div = page.body.find_all(name="span", attrs={"class": "content-time"})
-        article_div = page.body.find_all(name="p", attrs={})
-        title_article = page.title.text
-        file_name = __get_fileName_Opinion(date_div, title_article) + ".txt"
-        article_text = [article.get_text().strip() for article in article_div]
-        __download_article(file_name, article_text)
-    print("Done")
-
-
-def __get_articles_LaRazon(links):
-    for link in links:
-        response = requests.get(link)
-        page = BeautifulSoup(response.text, "html.parser")
-        article_div = page.body.find_all(name="div", attrs={"class": "article-body"})
-        article_text = [article.get_text().strip() for article in article_div]
-        title_article = page.title.text
-        file_name = __get_fileName_LaRazon(links[0], title_article) + ".txt"
-        __download_article(file_name, article_text)
-    print("Done")
-
-
-def __extract_LosTiempos(page):
-    def __get_divs_LosTiempos(page):
-        articles_divs1 = page.body.find_all(name="div", attrs={"class": "views-field-title"})
-        articles_divs2 = page.body.find_all(name="div", attrs={"class": "views-field-title term"})
-        articles_divs = articles_divs1 + articles_divs2
-        return articles_divs
-
-    articles_divs = __get_divs_LosTiempos(page)
+    articles_divs = __get_divs_opinion(page)
     links = __get_links(articles_divs)
-    __get_articles_LosTiempos(links)
+    __get_articles_opinion(links)
 
 
-def __extract_Opinion(page):
-    articles_divs = __get_divs_Opinion(page)
-    links = __get_links(articles_divs)
-    __get_articles_Opinion(links)
+def __extract_la_razon(page):
 
+    def __get_divs_la_razon(page_div):
+        article_divs = page_div.body.find_all(name="a", attrs={"class": "title"})
+        return article_divs
 
-def __extract_LaRazon(page):
+    def __get_links_la_razon(article_divs):
+        articles_links = []
+        for article in article_divs:
+            link = article.get("href")
+            articles_links.append(link)
+        return articles_links
+
+    def __get_file_name_la_razon(date_div, title_article):
+        match = re.search(r'\d{4}/\d{2}/\d{2}', date_div)
+        date = datetime.strptime(match.group(), '%Y/%m/%d')
+        date_str = date.strftime("%d %b %Y ")
+        article_title = (date_str + title_article)
+        article_title = __remove_punctuation(article_title)
+        return article_title
+
+    def __get_articles_la_razon(articles_links):
+        for link in articles_links:
+            response = requests.get(link)
+            article_page = BeautifulSoup(response.text, "html.parser")
+            article_div = article_page.body.find_all(name="div", attrs={"class": "article-body"})
+            article_text = [article.get_text().strip() for article in article_div]
+            title_article = article_page.title.text
+            file_name = __get_file_name_la_razon(link, title_article) + ".txt"
+            __download_article(file_name, article_text)
+        print("Done")
+
     articles_divs = __get_divs_la_razon(page)
     links = __get_links_la_razon(articles_divs)
-    __get_articles_LaRazon(links)
+    __get_articles_la_razon(links)
 
 
-def __extract_text(base_url):  # FIXME podemos usar únicamente inglés?
+def __extract_text(base_url):
     response = requests.get(base_url)
     page = BeautifulSoup(response.text, "html.parser")
     if base_url == "https://www.lostiempos.com/":
-        __extract_LosTiempos(page)
+        __extract_los_tiempos(page)
     elif base_url == "https://www.opinion.com.bo/blog/section/cochabamba":
-        __extract_Opinion(page)
+        __extract_opinion(page)
     elif base_url == "https://www.la-razon.com/sociedad/":
-        __extract_LaRazon(page)
+        __extract_la_razon(page)
