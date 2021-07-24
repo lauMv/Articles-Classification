afbfc53dded1b7dcdc04c0c03c9422971ddec3f3
laura Meneses <lmeneses58@gmail.com>
2021-07-24 00:15:00 -0400

    save articles to Db

--- repository/articles_schema.py
+++ repository/articles_schema.py
@@ -1,11 +1,10 @@
 from marshmallow import Schema, fields, EXCLUDE
 
+
 class ArticleSchema(Schema):
     id = fields.Integer(allow_none=True)
-    name = fields.Str(required=True, error_messages={"required": "An exoplanet needs at least a name"})
-    year_discovered = fields.Integer(allow_none=True)
-    light_years = fields.Float(allow_none=True)
-    mass = fields.Float(allow_none=True)
-    link = fields.Url(allow_none=True)
-    class Meta:
-        unknown = EXCLUDE
+    source_file_path = fields.Str(allow_none=False)
+    pre_processed_file_path = fields.Str(allow_none=False)
+    extraction_date = fields.Str(allow_none=False)
+    user_classification = fields.Str(allow_none=True)
+    model_classification = fields.Str(allow_none=True)
--- text_classifier/pre_processing.py
+++ text_classifier/pre_processing.py
@@ -1,5 +1,7 @@
 import os
+import re
 import spacy
+from repository import db
 
 nlp = spacy.load('es_core_news_md')
 
@@ -7,6 +9,26 @@ nlp = spacy.load('es_core_news_md')
 def execute():
     print("pre processing...")
     clean_articles()
+
+
+def extract_date(path):
+    extraction_date = None
+    los_tiempos_pattern = re.compile(r"^/actualidad/(cochabamba|ais)/(?P<date>\d{8})")
+    opinion_pattern = re.compile(r"^/articulo/(cochabamba|pais)/.*?/(?P<date>\d{8})")
+    la_razon_pattern = re.compile(r"^https://www\.la-razon\.com/nacional/(?P<date>\d{4}/\d{2}/\d{2})")
+    regex_match = los_tiempos_pattern.search(path)
+    extraction_date = regex_match.group('date')
+    if extraction_date is None:
+        regex_match = opinion_pattern.search(path)
+        extraction_date = regex_match.group('date')
+    if extraction_date is None:
+        regex_match = la_razon_pattern.search(path)
+        extraction_date = regex_match.group('date')
+    return extraction_date
+
+
+def save_on_db(source_file_path, pre_processed_file_path, extraction_date, user_classification, model_classification):
+    db.create(source_file_path, pre_processed_file_path, extract_date(source_file_path), None, None)
 
 
 def clean_articles():
@@ -17,7 +39,7 @@ def clean_articles():
         with open(path + file, "r", encoding="utf-8", errors="ignore") as article_file:
             article = article_file.read()
             cleaned = clean_text(article.replace("\n", ""))
-            save_cleaned_article(file, cleaned)
+            save_cleaned_article(file, cleaned, path)
     print("Clean all articles")
 
 
@@ -31,9 +53,10 @@ def clean_text(text):
     return cleaned_text
 
 
-def save_cleaned_article(name, clean_article):
+def save_cleaned_article(name, clean_article, path):
     name_ = "cleaned " + name
     path_ = os.path.join(os.getenv("TEXT_CLASSIFIER_DATA"), "cleaned_articles", name_)
+    save_on_db(path, path_,)
     if not os.path.exists(path_):
         print("Guardando artículo limpio en {}".format(path_))
         with open(path_, "a+", encoding="utf-8") as file:
